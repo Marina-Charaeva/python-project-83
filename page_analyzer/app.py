135e0457027f737264e6bb30f1985610dbcf0901
Marina Charaeva <panivan1298@mail.ru>
2025-10-05 16:37:23 +0000

    Разворачивание окружения

---
+++ page_analyzer/app.py
@@ -0,0 +1,17 @@
+import os
+
+
+import requests
+from dotenv import load_dotenv
+from flask import (
+    Flask,
+)
+
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+if __name__ == '__main__':
+    app.run()
